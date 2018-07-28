3f0c338ab7526524670db1beb48b5be858596dc5
MindaugasNavickas <33937291+MindaugasNavickas@users.noreply.github.com>
2018-07-28 16:16:55 +0100

    Revert "Updated personalstats view"
    This reverts commit fee82fb0dd3ecf552af63793eadef7ee54e57f05.

--- src/eco/quester/custom_frame/CustomFrame.java
+++ src/eco/quester/custom_frame/CustomFrame.java
@@ -25,6 +25,8 @@ public class CustomFrame extends JFrame {
 	public static JPanel personalStatisticsView;
 	public static JPanel questsByExpRewardsView;
 	public static JPanel questsByStatisticsView;
+	public static CTextArea nameTextArea;
+	public static CTextField nameTextField;
 	
 	public CustomFrame() {
 		
@@ -44,6 +46,11 @@ public class CustomFrame extends JFrame {
 
 		addLeftListSelectionOptions();
 		initSelectedOptionPanels();
+		
+		if(personalStatisticsView.isVisible()) {
+			inputField();
+			inputTextName();
+		}
 		
 
 		
@@ -94,6 +101,20 @@ public class CustomFrame extends JFrame {
 		}
 		
 		add(leftListPanel);
+	}
+	
+	private void inputField() {
+		nameTextField = new CTextField();
+		nameTextField.setBackground(new Color(60,60,60));
+		nameTextField.setBounds(Settings.FRAME_WIDTH/2, Settings.FRAME_HEIGHT/8, 150,25);
+		add(nameTextField);
+	}
+	
+	private void inputTextName() {
+		nameTextArea = new CTextArea("Player Name");
+		nameTextArea.setBackground(Settings.BACKGROUND_COLOR);
+		nameTextArea.setBounds(Settings.FRAME_WIDTH/4, Settings.FRAME_HEIGHT/8, 150,25);
+		add(nameTextArea);
 	}
 	
 	private void MouseListener() {	//Here down allows the custom frame to be moved like a normal frame
