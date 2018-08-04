e097b19b24f1083e92c054ee0ba61d74385f5000
MindaugasNavickas <33937291+MindaugasNavickas@users.noreply.github.com>
2018-08-04 17:55:51 +0100

    Length tab (not done)

---
+++ src/eco/quester/listeners/QuestLengthActionListener.java
@@ -0,0 +1,42 @@
+package eco.quester.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import eco.quester.custom_objects.CCheckBoxMenuItem;
+import eco.quester.menus.Difficulty;
+import eco.quester.menus.Length;
+
+public class QuestLengthActionListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		switch(e.getActionCommand()) {
+		
+		case "Short":
+			applyAction(Length.lengthShort);
+			break;
+			
+		case "Medium":
+			applyAction(Length.lengthMedium);
+			break;
+			
+		case "Long":
+			applyAction(Length.lengthLong);
+			break;
+			
+		
+		}
+		
+	}
+	
+	public void applyAction(CCheckBoxMenuItem item) {
+		if(item.isSelected())
+			Difficulty.difficultyMap.put(item.getText().toLowerCase().replaceAll(" ", ""), true);
+		else
+			Difficulty.difficultyMap.put(item.getText().toLowerCase().replaceAll(" ", ""), false);
+	}
+	
+	
+}
--- src/eco/quester/menus/Length.java
+++ src/eco/quester/menus/Length.java
@@ -1,23 +1,58 @@
 package eco.quester.menus;
 
+import java.util.HashMap;
+
 import eco.quester.custom_objects.CCheckBoxMenuItem;
 import eco.quester.custom_objects.CMenu;
 import eco.quester.custom_objects.CMenuBar;
+import eco.quester.listeners.QuestLengthActionListener;
 
 public class Length {
 	
+	public static CCheckBoxMenuItem lengthShort;
+	private static boolean shortLength = false;
+	public static CCheckBoxMenuItem lengthMedium;
+	private static boolean mediumLength = false;
+	public static CCheckBoxMenuItem lengthLong;
+	private static boolean longLength = false;
+	
+	public static HashMap<String, Boolean> lengthMap;
+	
+	private static void fillLengthMap() {
+		// TODO Auto-generated method stub
+		lengthMap = new HashMap<String, Boolean>();
+		lengthMap.put("Short".toLowerCase(), shortLength);
+		lengthMap.put("Medium".toLowerCase(), mediumLength);
+		lengthMap.put("Long".toLowerCase(), longLength);
+		
+	}
+	
+	
 	public static void initQuestLength(CMenuBar menuBar) {
+		fillLengthMap();
 		
 		CMenu questLengthMenu = new CMenu("Quest Length");
 	
-		CCheckBoxMenuItem shortLength = new CCheckBoxMenuItem("Short");
-		questLengthMenu.add(shortLength);
+		lengthShort = new CCheckBoxMenuItem("Short");
+		lengthShort.addActionListener(new QuestLengthActionListener());
+		questLengthMenu.add(lengthShort);
 		
-		CCheckBoxMenuItem mediumLength = new CCheckBoxMenuItem("Medium");
-		questLengthMenu.add(mediumLength);
+		lengthMedium = new CCheckBoxMenuItem("Medium");
+		lengthMedium.addActionListener(new QuestLengthActionListener());
+		questLengthMenu.add(lengthMedium);
 		
-		CCheckBoxMenuItem longLength = new CCheckBoxMenuItem("Long");
-		questLengthMenu.add(longLength);
+		lengthLong = new CCheckBoxMenuItem("Long");
+		lengthLong.addActionListener(new QuestLengthActionListener());
+		questLengthMenu.add(lengthLong);
+		
+//		CCheckBoxMenuItem shortLength = new CCheckBoxMenuItem("Short");
+//		questLengthMenu.add(shortLength);
+//		
+//		CCheckBoxMenuItem mediumLength = new CCheckBoxMenuItem("Medium");
+//		questLengthMenu.add(mediumLength);
+//		
+//		CCheckBoxMenuItem longLength = new CCheckBoxMenuItem("Long");
+//		questLengthMenu.add(longLength);
 		
 		menuBar.add(questLengthMenu);
 		
